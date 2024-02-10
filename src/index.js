a15883e8b05782b636ccc6daded01a300b712bc7
Jonathan Gomez <rosemaria011@outlook.com>
2024-02-09 23:43:36 -0800

    fixed

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import { fileURLToPath } from "url";
 import { createServer as createHttpsServer } from "node:https";
 import { createServer as createHttpServer } from "node:http";
 import { readFileSync, existsSync } from "node:fs";
+import express from "express";
 import serveStatic from "serve-static";
-
+import { hostname } from "node:os";
 const bare = createBareServer("/bare/");
 const serve = serveStatic(fileURLToPath(new URL("../static/", import.meta.url)), { fallthrough: false });
 var server;
 server = createHttpServer();
-
 server.on("request", (req, res) => {
   if(bare.shouldRoute(req)) bare.routeRequest(req, res); else {
     serve(req, res, (err) => {
@@ -25,16 +25,37 @@ server.on("upgrade", (req, socket, head) => {
   if(bare.shouldRoute(req, socket, head)) bare.routeUpgrade(req, socket, head); else socket.end();
 });
 
-server.on('listening', () => {
-  const addr = server.address();
+let port = parseInt(process.env.PORT || "");
 
-  console.log(`Server running on port ${addr.port}`)
-	console.log('');
-  console.log('You can now view it in your browser.')
-  /* Code for listing IPS from website-aio */
-	console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
-	try { console.log(`On Your Network: http://${address.ip()}:${addr.port}`); } catch (err) {/* Can't find LAN interface */};
-	if(process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
+if (isNaN(port)) port = 8080;
+
+server.on("listening", () => {
+  const address = server.address();
+
+  // by default we are listening on 0.0.0.0 (every interface)
+  // we just need to list a few
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(`\thttp://${hostname()}:${address.port}`);
+  console.log(
+    `\thttp://${
+      address.family === "IPv6" ? `[${address.address}]` : address.address
+    }:${address.port}`
+  );
 });
 
-server.listen({ port: (process.env.PORT || 8080) })
+// https://expressjs.com/en/advanced/healthcheck-graceful-shutdown.html
+process.on("SIGINT", shutdown)
+process.on("SIGTERM", shutdown)
+
+function shutdown() {
+  console.log('SIGTERM signal received: closing HTTP server')
+  server.close(() => {
+    console.log('Server closed.');
+    process.exit(0);
+  });
+}
+
+server.listen({
+  port,
+});
